607f67006651fc415ec2f366cba9e908f3566319
sillitoe <ian@sillit.com>
2018-07-27 19:31:50 +0100

    add view for details

--- django/djangorest/api/urls.py
+++ django/djangorest/api/urls.py
@@ -1,11 +1,12 @@
 # api/urls.py
 
 from django.conf.urls import url, include
-from .views import CreateView
+from .views import CreateView, DetailsView
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = {
     url(r'^bucketlists/$', CreateView.as_view(), name="create"),
+    url(r'^bucketlists/(?P<pk>[0-9]+)/$', DetailsView.as_view(), name="details"),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
