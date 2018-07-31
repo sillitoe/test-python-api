05c0b4099b85169c5ccf6ce22fa04c29deccfeea
Ian Sillitoe <ian@sillit.com>
2018-07-31 16:53:13 +0200

    test SelectTemplateTask

--- django/djangorest/api/tests.py
+++ django/djangorest/api/tests.py
@@ -1,7 +1,7 @@
 
 from django.test import TestCase
 from django.urls import reverse
-from .models import Bucketlist
+from .models import SelectTemplateTask
 
 from rest_framework.test import APIClient
 from rest_framework import status
@@ -9,18 +9,18 @@ from rest_framework import status
 # Create your tests here.
 
 class ModelTestCase(TestCase):
-    """This class defines the test suite for the bucketlist model."""
+    """This class defines the test suite for the select template model."""
 
     def setUp(self):
         """Define the test client and other test variables."""
-        self.bucketlist_name = "Write world class code"
-        self.bucketlist = Bucketlist(name=self.bucketlist_name)
-    
-    def test_model_can_create_a_bucketlist(self):
-        """Test the bucketlist model can create a bucketlist."""
-        old_count = Bucketlist.objects.count()
-        self.bucketlist.save()
-        new_count = Bucketlist.objects.count()
+        self.template_query_fasta = ">query\nAPKGAPKGAPKGAPKGAPKGAKPGAKGKPAKGPAKGPAKGAGPKAGPKAGPKAPGKAAGPK\n"
+        self.template_task = SelectTemplateTask(fasta=self.template_query_fasta)
+
+    def test_model_can_create_a_task(self):
+        """Test the tasklist model can create a task."""
+        old_count = SelectTemplateTask.objects.count()
+        self.template_task.save()
+        new_count = SelectTemplateTask.objects.count()
         self.assertNotEqual(old_count, new_count)
 
 class ViewTestCase(TestCase):
@@ -29,42 +29,31 @@ class ViewTestCase(TestCase):
     def setUp(self):
         """Define the test client and other test variables."""
         self.client = APIClient()
-        self.bucketlist_data = {'name': 'Go to Ibiza'}
+        self.template_task_data = {'fasta': ">query\nGPKASPGAPKAPKAPKGPKAGPAKGPKAPSKGPSAKPGKASPKGPAASPGK\n"}
         self.response = self.client.post(
-            reverse('create'),
-            self.bucketlist_data,
+            reverse('create_selecttemplate'),
+            self.template_task_data,
             format="json"
         )
-    
-    def test_api_can_create_a_bucketlist(self):
-        """Test the api has bucket creation capability"""
+
+    def test_api_can_create_a_task(self):
+        """Test the api has task creation capability"""
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
 
-    def test_api_can_get_a_bucketlist(self):
-        """Test the api can get a given bucketlist."""
-        bucketlist = Bucketlist.objects.get()
+    def test_api_can_get_a_task(self):
+        """Test the api can get a given tasklist."""
+        task = SelectTemplateTask.objects.get()
         response = self.client.get(
-            reverse('details',
-            kwargs={'pk': bucketlist.id}), format="json")
-
+            reverse('details_selecttemplate',
+            kwargs={'pk': task.id}), format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertContains(response, bucketlist)
+        self.assertContains(response, task.id)
 
-    def test_api_can_update_bucketlist(self):
-        """Test the api can update a given bucketlist."""
-        bucketlist = Bucketlist.objects.get()
-        change_bucketlist = {'name': 'Something new'}
-        res = self.client.put(
-            reverse('details', kwargs={'pk': bucketlist.id}),
-            change_bucketlist, format='json'
-        )
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-
-    def test_api_can_delete_bucketlist(self):
-        """Test the api can delete a bucketlist."""
-        bucketlist = Bucketlist.objects.get()
+    def test_api_can_delete_task(self):
+        """Test the api can delete a task."""
+        task = SelectTemplateTask.objects.get()
         response = self.client.delete(
-            reverse('details', kwargs={'pk': bucketlist.id}),
+            reverse('details_selecttemplate', kwargs={'pk': task.id}),
             format='json',
             follow=True)
 
